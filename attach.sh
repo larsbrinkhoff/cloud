a008e1e1636ed28ea48394f4848951720e3d2d42
Lars Brinkhoff <lars@nocrew.org>
2022-06-04 21:31:32 +0200

    Set window title to host name

--- attach.sh
+++ attach.sh
@@ -6,8 +6,10 @@ cd $HOME/bin
 . ./conf.sh
 
 PORT="$1"
-OPTIONS="-o ServerAliveInterval=5 -t -l $USER -p $PORT"
+HOST=`ssh -l $USER $SERVER grep \"start $PORT\" .nodes | tail -1 | cut -d' ' -f3`
+printf "\033]0;$HOST\007"
 
+OPTIONS="-o ServerAliveInterval=5 -t -l $USER -p $PORT"
 while :; do
     ssh $OPTIONS "$SERVER" screen -U -S cloud -x
     sleep 2
